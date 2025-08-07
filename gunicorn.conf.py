a7762690560e7147312299d47a0c8fe5946df61d
Krishna <131504859+krishn-shetty@users.noreply.github.com>
2025-08-07 20:08:37 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,13 @@
 import eventlet
 eventlet.monkey_patch()
 
+import os
+
 bind = f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"
 workers = 1
 worker_class = "eventlet"
 timeout = 30
 keepalive = 2
 loglevel = "info"
+accesslog = "-"
+errorlog = "-"

310c273343e404166e8c20123f7ce245951f807b
Krishna <131504859+krishn-shetty@users.noreply.github.com>
2025-08-07 20:40:43 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,9 @@
-import eventlet
-eventlet.monkey_patch()
+import multiprocessing
 
-import os
-
-bind = f"0.0.0.0:{int(os.environ.get('PORT', 10000))}"
-workers = 1
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "eventlet"
+worker_connections = 1000
 timeout = 30
 keepalive = 2
 loglevel = "info"
-accesslog = "-"
-errorlog = "-"

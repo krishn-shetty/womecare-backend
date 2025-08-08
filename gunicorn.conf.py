e28f4204e44faa63c7808e1e0696140be5834d2e
Krishna <131504859+krishn-shetty@users.noreply.github.com>
2025-08-08 13:15:08 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', 8000)}"
-workers = 2  # Fixed to 2 for resource efficiency on Render
+workers = 1  # Fixed to 2 for resource efficiency on Render
 worker_class = "eventlet"
 worker_connections = 1000
 timeout = 30
